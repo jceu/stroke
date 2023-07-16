5156db247ec563175b148bb28b9eea2385191a7e
jceu <137873837+jceu@users.noreply.github.com>
2023-07-16 18:03:09 +0800

    Update streamlit-web-deploy.py

--- streamlit-web-deploy.py
+++ streamlit-web-deploy.py
@@ -106,12 +106,6 @@ else:
 st.subheader(prediction_stroke)
 
 st.subheader('Prediction Probability')
-labels = ['0', '1']
-sizes = [prediction_proba[:,0]]
-colors = ['red', 'yellow']
-fig, ax = plt.subplots()
-ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
-ax.axis('equal')
-st.pyplot(fig)
 
-st.write(prediction_proba)
+
+st.write(prediction_proba[:,0])
