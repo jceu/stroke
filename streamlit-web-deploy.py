0c320c754e07ebfc6e33d327bf14b9e0df385ec5
jceu <137873837+jceu@users.noreply.github.com>
2023-07-16 18:23:59 +0800

    Update streamlit-web-deploy.py

--- streamlit-web-deploy.py
+++ streamlit-web-deploy.py
@@ -112,5 +112,5 @@ colors = ['red', 'yellow']
 fig, ax = plt.subplots()
 ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
 ax.axis('equal')
-fig.set_facecolor('lightgray')
+fig.set_facecolor('gray')
 st.pyplot(fig)
