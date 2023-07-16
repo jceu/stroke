a17cf172ea799282024266300ae211dceab7ee00
jceu <137873837+jceu@users.noreply.github.com>
2023-07-17 02:08:01 +0800

    Update streamlit-web-deploy.py

--- streamlit-web-deploy.py
+++ streamlit-web-deploy.py
@@ -153,6 +153,7 @@ with st.container():
         st.write("(Open web page in wide mode to view pie chart better and have a better overall experience)")
         
     with right_column:
+        st.write("#")
         st.header('Prediction: ')
         stroke_type = np.array([0, 1])
         if stroke_type[prediction] == 1:
